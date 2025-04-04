2d2c2050dae209f24d4ea7f579cdbcf8332e0e22
Nikoli Dryden <ndryden@users.noreply.github.com>
2025-04-03 17:04:19 -0700

    Add license headers.

--- yogrt/__init__.py
+++ yogrt/__init__.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2014-2025, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+# Written by the LBANN Research Team (B. Van Essen, et al.) listed in
+# the CONTRIBUTORS file. See the top-level LICENSE file for details.
+#
+# LLNL-CODE-697807.
+# All rights reserved.
+#
+# This file is part of LBANN: Livermore Big Artificial Neural Network
+# Toolkit. For details, see http://software.llnl.gov/LBANN or
+# https://github.com/LBANN and https://github.com/LLNL/LBANN.
+#
+# SPDX-License-Identifier: (Apache-2.0)
+
 """Determine remaining job time from a resource manager.
 
 This provides an interface to libyogrt ("Your One Get Remaining Time
